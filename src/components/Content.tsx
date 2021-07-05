17f88fa40c8f8f6a67c547ac61c91d82cfeaa296
Claudio Couto <claudiocouto3101@gmail.com>
2021-07-04 21:39:28 -0300

    Whatchme Finalized

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,41 @@
-export function Content() {
+import { MovieCard } from '../components/MovieCard';
+import '../styles/content.scss';
+
+interface props {
+  selectedGenre: {
+    title: string;
+  };
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Runtime: string;
+    Ratings: Array<{
+      Value: string;
+    }>;
+  }>
+}
+
+export function Content({selectedGenre, movies}: props) {
   // Complete aqui
+  
+  return(
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+    )
 }
